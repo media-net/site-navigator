3598f0a9be63446e05d7e0b389a5933567e928c7
vaishnavip62 <vaishnavi.p@media.net>
2025-12-01 15:43:10 +0530

    dist lander created

--- microsite/public/js/mixpanelSiteProperties.js
+++ microsite/public/js/mixpanelSiteProperties.js
@@ -1,6 +1,6 @@
-const MIXPANEL_CUSTOM_LIB_URL = 'https://t.privatesearchmode.com/lib.min.js';
-const MIXPANEL_API_HOST = 'https://t.privatesearchmode.com';
-const extensionName = 'privatesearch';
+const MIXPANEL_CUSTOM_LIB_URL = 'https://t.sitenavigator.co/lib.min.js';
+const MIXPANEL_API_HOST = 'https://t.sitenavigator.co';
+const extensionName = 'sitenavigator';
 const mixpanelCustomProperties = {};
 mixpanelCustomProperties['Extension Name'] = extensionName;
 mixpanelCustomProperties['param1'] = 160005;
